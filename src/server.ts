62090da9bbd1c2567f4e6d93d90d60bf9f836ece
frantecbh <franciscomenezesbh@gmail.com>
2024-04-24 20:55:28 -0300

    separando as rotas

--- src/server.ts
+++ src/server.ts
@@ -1,44 +1,17 @@
 import fastify from 'fastify'
-import z from 'zod'
-import { prisma } from './database/prisma'
-import { generateSlug } from './utils/generate-slug'
+import {
+  serializerCompiler,
+  validatorCompiler,
+} from 'fastify-type-provider-zod'
+import { createEvent } from './routes/create-events'
 
 const app = fastify()
 
-app.post('/events', async (request, reply) => {
-  const createEventSchema = z.object({
-    title: z.string().min(4),
-    details: z.string().nullable(),
-    maximunAttendees: z.number().int().positive().nullable(),
-  })
+app.register(createEvent)
 
-  const { title, details, maximunAttendees } = createEventSchema.parse(
-    request.body,
-  )
-
-  const slug = generateSlug(title)
-
-  const eventWithSameSlug = await prisma.event.findUnique({
-    where: {
-      slug,
-    },
-  })
-
-  if (eventWithSameSlug) {
-    throw new Error('Another event with same title already exists.')
-  }
-
-  const event = await prisma.event.create({
-    data: {
-      title,
-      details,
-      maximunAttendees,
-      slug,
-    },
-  })
-
-  return reply.status(201).send({ eventID: event.id })
-})
+// Add schema validator and serializer
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
 
 app.listen({ port: 3333 }).then(() => {
   console.log('HTTP server running')
